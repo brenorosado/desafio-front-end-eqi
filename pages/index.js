fd54c73bcc76f0e4cc5dca8ef36e6309e74d72b3
Breno Rosado <brenorosado.cjr@gmail.com>
2022-02-07 23:52:35 -0300

    Campos do formulario criados e ajustes de estilo feitos

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,81 @@
-import { MainContent, RadioInputsDiv, RadioInput, RadioLabel } from "../src/styles/homepage";
-import { BsCheck } from "react-icons/bs";
+import { MainContent, RadioInputsDiv, RadioInput, RadioLabel, FormElement, NumberInput } from "../src/styles/homepage";
+import { BsCheck, BsInfoCircle } from "react-icons/bs";
 import { useState } from "react";
 
 const Home = () => {
-  const [incomeType, setIncomeType] = useState('Gross')
+  const [incomeType, setIncomeType] = useState('Gross');
+  const [indexingType, setIndexingType] = useState('Post')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 
   return (
     <MainContent>
       <h1>Simulador de Investimentos</h1>
       <section>
-        <div>
-          <h2>Simulador</h2>
-          <form>
-              <span>Rendimento</span>
+        <h2>Simulador</h2>
+        <form onSubmit={handleSubmit}>
+          <FormElement>
+            <span>Rendimento <BsInfoCircle /></span>
             <RadioInputsDiv>
-              <RadioInput type="radio" id="grossIncome" name="incomeType" value="Gross" className="radio__input" checked onClick={(e) => setIncomeType('Gross')} />
-              <RadioLabel for="grossIncome" className="radio__label" 
+              <RadioInput type="radio" id="grossIncome" name="incomeType" value="Gross" onChange={(e) => setIncomeType('Gross')} />
+              <RadioLabel for="grossIncome"
                 background={incomeType === 'Gross' ? '#ed8e53' : '#efefef'}
                 color={incomeType === 'Gross' ? 'white' : 'black'}>
-                {incomeType === 'Gross' ? <><BsCheck/>Bruto</> : 'Bruto'}
+                {incomeType === 'Gross' ? <><BsCheck />Bruto</> : 'Bruto'}
               </RadioLabel>
-              <RadioInput type="radio" id="liquidIncome" name="incomeType" value="Liquid" className="radio__input" onClick={(e) => setIncomeType('Liquid')} />
-              <RadioLabel for="liquidIncome" className="radio__label"
+              <RadioInput type="radio" id="liquidIncome" name="incomeType" value="Liquid" onChange={(e) => setIncomeType('Liquid')} />
+              <RadioLabel for="liquidIncome"
                 background={incomeType === 'Liquid' ? '#ed8e53' : '#efefef'}
                 color={incomeType === 'Liquid' ? 'white' : 'black'}>
-                {incomeType === 'Liquid' ? <><BsCheck/>Líquido</> : 'Líquido'}
+                {incomeType === 'Liquid' ? <><BsCheck />Líquido</> : 'Líquido'}
               </RadioLabel>
             </RadioInputsDiv>
-          </form>
-        </div>
+          </FormElement>
+          <FormElement>
+            <span>Tipos de indexação <BsInfoCircle /></span>
+            <RadioInputsDiv>
+              <RadioInput type="radio" id="preIndexing" name="indexingType" value="Pre" onChange={(e) => setIndexingType('Pre')} />
+              <RadioLabel for="preIndexing"
+                background={indexingType === 'Pre' ? '#ed8e53' : '#efefef'}
+                color={indexingType === 'Pre' ? 'white' : 'black'}>
+                {indexingType === 'Pre' ? <><BsCheck />PRÉ</> : 'PRÉ'}
+              </RadioLabel>
+              <RadioInput type="radio" id="postIncome" name="indexingType" value="Post" onChange={(e) => setIndexingType('Post')} />
+              <RadioLabel for="postIncome"
+                background={indexingType === 'Post' ? '#ed8e53' : '#efefef'}
+                color={indexingType === 'Post' ? 'white' : 'black'}>
+                {indexingType === 'Post' ? <><BsCheck />PÓS</> : 'PÓS'}
+              </RadioLabel>
+              <RadioInput type="radio" id="fixedIncome" name="indexingType" value="Fixed" onChange={(e) => setIndexingType('Fixed')} />
+              <RadioLabel for="fixedIncome"
+                background={indexingType === 'Fixed' ? '#ed8e53' : '#efefef'}
+                color={indexingType === 'Fixed' ? 'white' : 'black'}>
+                {indexingType === 'Fixed' ? <><BsCheck />FIXADO</> : 'FIXADO'}
+              </RadioLabel>
+            </RadioInputsDiv>
+          </FormElement>
+          <FormElement>
+            <label>Aporte Inicial</label>
+            <NumberInput type="number" id="inicialContribution" min="1"/>
+          </FormElement>
+          <FormElement>
+            <label>Aporte Mensal</label>
+            <NumberInput type="number" id="inicialContribution" min="1"/>
+          </FormElement>
+          <FormElement>
+            <label>Prazo (em meses)</label>
+            <NumberInput type="number" id="inicialContribution" min="1"/>
+          </FormElement>
+          <FormElement>
+            <label>Rentabilidade</label>
+            <NumberInput type="number" id="inicialContribution" min="1"/>
+          </FormElement>
+          <button>Limpar campos</button>
+          <input type="submit" value="Simular"/>
+        </form>
       </section>
     </MainContent>
   );
--- src/styles/homepage.js
+++ src/styles/homepage.js
@@ -3,13 +3,36 @@ import styled from "styled-components";
 export const MainContent = styled.main`
     background-color: #efefef;
     width: 100%;
+    padding: 30px;
+
+    h1 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    form {
+        display: grid;
+        grid-template-columns: repeat(2, 220px);
+        grid-column-gap: 50px;
+        grid-row-gap: 20px;
+    }
+
+    span {
+        display: flex;
+        justify-content: space-between;
+        margin: 10px 0px;
+    }
+`;
+
+export const FormElement = styled.div`
+    display: flex;
+    flex-direction: column;
 `;
 
 export const RadioInputsDiv = styled.div`
     display: inline-flex;
     overflow: hidden;
-    border-radius: 15px;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
     border: 1px solid black;
 `;
 
@@ -24,12 +47,30 @@ export const RadioLabel = styled.label`
     background: ${props => props.background};
     cursor: pointer;
     transition: background 0.1s;
-    width: 80px;
+    width: 110px;
     display: flex;
     justify-content: center;
     align-items: center;
 
     :not(:last-of-type) {
         border-right: 1px solid black;
+    }
+`;
+
+export const NumberInput = styled.input`
+    padding: 10px;
+    border: none;
+    border-bottom: 1px solid gray;
+    background-color: #efefef;
+    font-size: 16px;
+
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    :focus {
+        outline: none;
     }
 `;
